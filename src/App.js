30e728ddf4f034707f1bc1d9459944807c8ad540
Onuh Uche <onuhblaze@gmail.com>
2023-04-29 12:54:32 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
 function App() {
-
-  const [searchField, setSearchField] = useState("");
+  const [searchfield, setSearchfield] = useState("");
   const [robotsArray, setRobotsArray] = useState(robots);
+
+  const onChange = (e) => {
+    setSearchfield(e.target.value);
+    const filteredRobot = robotsArray.filter((robot) =>
+      robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase())
+    );
+    setRobotsArray(filteredRobot);
+  };
 
   return (
     <div className="tc">
       <h1 className="h1">RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
+      <SearchBox onChange={onChange} />
+      <CardList robots={robotsArray} />
     </div>
   );
 }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,16 +1,10 @@
-import React, { useRef } from "react";
+// import React, { useRef} from "react";
 
-const SearchBox = () => {
-  const inputRef = useRef();
-
-  const onChange = () => {
-    const value = inputRef.current.value;
-  };
+const SearchBox = ({ onChange }) => {
 
   return (
     <div className="pa2">
       <input
-        ref={inputRef}
         type="text"
         placeholder="Search"
         className="pa3 ba b--green bg-lightest-blue "
