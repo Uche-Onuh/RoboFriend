6ac7e56b7927b5303c97d2f0980369666d0257bb
Onuh Uche <onuhblaze@gmail.com>
2023-04-30 08:23:22 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { useState, useEffect } from "react";
+import { useMemo, useState } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
 function App() {
-  const [searchfield, setSearchfield] = useState("");
-  const [robotsArray, setRobotsArray] = useState(robots);
+  const robotsArray = robots;
+  const [query, setQuery] = useState("");
 
-  const onChange = (e) => {
-    setSearchfield(e.target.value);
-    const filteredRobot = robotsArray.filter((robot) =>
-      robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase())
-    );
-    setRobotsArray(filteredRobot);
-  };
+
+
+   const filteredRobot = useMemo(() => (
+    robotsArray.filter((robot) =>
+      robot.name.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+    )
+   ), [robotsArray, query])
+
+   
 
   return (
     <div className="tc">
       <h1 className="h1">RoboFriends</h1>
-      <SearchBox onChange={onChange} />
-      <CardList robots={robotsArray} />
+      <SearchBox onChange={(e) => setQuery(e.target.value)} value={query} />
+      <CardList robots={filteredRobot} />
     </div>
   );
 }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,10 +1,10 @@
 // import React, { useRef} from "react";
 
-const SearchBox = ({ onChange }) => {
-
+const SearchBox = ({ onChange, value }) => {
   return (
     <div className="pa2">
       <input
+        value={value}
         type="text"
         placeholder="Search"
         className="pa3 ba b--green bg-lightest-blue "
